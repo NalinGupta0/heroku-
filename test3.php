d3fa3decf495094e358093bf2ff4de20123e3ee6
NalinGupta0 <48588594+NalinGupta0@users.noreply.github.com>
2021-04-02 08:59:39 +0530

    Add files via upload

---
+++ test3.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Heroku Test 3</title>
+</head>
+<body bgcolor="green">
+
+<?php
+echo  "<p style='color:blue; font-size: 50px; text-align: center;'>".'This is Heroku Test 3'."</p>";
+echo "</br>";
+echo  "<p style='color:red; font-size: 25px; text-align: center;'>" .'This is a sample 3 website used for Heroku Deployment. It is created to show how to deploy sites to heroku. It is made by Nalin Gupta.'."</p>";
+?>
+
+</body>
+</html>
