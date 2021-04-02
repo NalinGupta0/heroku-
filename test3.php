9ac916e3b6c66a4616843c28552ed3b1a31d25f2
NalinGupta0 <48588594+NalinGupta0@users.noreply.github.com>
2021-04-02 09:27:32 +0530

    Update test3.php

--- test3.php
+++ test3.php
@@ -1,8 +1,4 @@
-<html>
-<head>
-  <title>Heroku Test 3</title>
-</head>
-<body bgcolor="green">
+
 
 <?php
 echo  "<p style='color:blue; font-size: 50px; text-align: center;'>".'This is Heroku Test 3'."</p>";
@@ -10,5 +6,3 @@ echo "</br>";
 echo  "<p style='color:red; font-size: 25px; text-align: center;'>" .'This is a sample 3 website used for Heroku Deployment. It is created to show how to deploy sites to heroku. It is made by Nalin Gupta.'."</p>";
 ?>
 
-</body>
-</html>
