9ff31327df448d259a3a66d9364b7da622cd50da
NalinGupta0 <48588594+NalinGupta0@users.noreply.github.com>
2021-04-02 09:34:31 +0530

    Delete test3.php

--- test3.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php
-echo  "<p style='color:blue; font-size: 50px; text-align: center;'>".'This is Heroku Test 3'."</p>";
-echo "</br>";
-echo  "<p style='color:red; font-size: 25px; text-align: center;'>" .'This is a sample 3 website used for Heroku Deployment. It is created to show how to deploy sites to heroku. It is made by Nalin Gupta.'."</p>";
-?>
-
